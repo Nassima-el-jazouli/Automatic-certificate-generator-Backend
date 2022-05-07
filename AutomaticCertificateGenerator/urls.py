fd35a9b7560d43fafbc6d78256d50b4cd9194a0a
Nassima EL JAZOULI <nassima.eljazouli@etu.uae.ac.ma>
2022-05-07 23:51:03 +0000

    another commit

--- AutomaticCertificateGenerator/urls.py
+++ AutomaticCertificateGenerator/urls.py
@@ -1,8 +1,15 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import re_path
 from django.conf.urls import include
+from django.conf.urls.static import static
 
 urlpatterns = [
     re_path('admin/', admin.site.urls),
     re_path(r'^', include('Certificate.urls')),
 ]
+
+
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Certificate/models.py
+++ Certificate/models.py
@@ -6,7 +6,7 @@ class Certificate(models.Model):
     Title = models.CharField(max_length=70, blank=False, default='')
     Description = models.CharField(max_length=250, blank=False, default='')
     Date = models.DateField(blank=False)
-    myImage = models.FileField(upload_to ='media/myImages/', max_length=254, blank=False)
+    myImage = models.ImageField(upload_to ='media/myImages/', max_length=254, blank=False)
     myFile = models.FileField(upload_to ='media/myFiles/', max_length=254, blank=False)
 
     def __str__(self):
